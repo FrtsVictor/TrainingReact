0a6dcc5c3149091cd757751c88da613adf2f5980
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-22 19:52:57 -0300

    Sorting out TasksList Component

---
+++ todo_List_lint/src/component/Tasks/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import './Tasks.css';
+
+export default function Tasks({ tasks, handleDelete, handleEdit }) {
+  return (
+    <ul className="tasks">
+      { tasks.map((task, index) => (
+        <li key={index}>
+          {' '}
+          {task}
+          <span>
+            <FaEdit
+              onClick={(e) => handleEdit(e, index)}
+              className="edit"
+            />
+            <FaWindowClose
+              onClick={(e) => handleDelete(e, index)}
+              className="delete"
+            />
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+Tasks.propTypes = {
+  handleDelete: PropTypes.func.isRequired,
+  handleEdit: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired,
+};
