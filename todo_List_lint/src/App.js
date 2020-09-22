56f932cc890dc790442599dcc75108e277377dbd
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-22 06:41:36 -0300

    Started Project

---
+++ todo_List_lint/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; // sempre primeira linha pra usar o jsx
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <h1>Training :) </h1>
+      <p>Reactzin</p>
+    </>
+  );
+}
