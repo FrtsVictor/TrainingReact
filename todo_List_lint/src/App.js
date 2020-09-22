2303180e2c2b575757b964e968c0082c4028b722
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-22 07:27:00 -0300

    Main Styles

--- todo_List_lint/src/App.js
+++ todo_List_lint/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'; // sempre primeira linha pra usar o jsx
+import Main from './component/Main';
+
 import './App.css';
 
+// functions without state return jsx
+// components stateful are classes and need method render
 export default function App() {
   return (
-    <>
-      <h1>Training :) </h1>
-      <p>Reactzin</p>
-    </>
+    <Main />
   );
 }
