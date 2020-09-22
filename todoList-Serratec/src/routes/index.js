f8c3659f2567c4deab8fef71cde867cf2eca9af8
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-21 21:03:26 -0300

    Added Axios

--- todoList-Serratec/src/routes/index.js
+++ todoList-Serratec/src/routes/index.js
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Tarefas from '../pages/Tarefas';
 
+
+
 const Routes = () => 
     //switch para gerenciar e n enviar todas rotas junto
-    <Switch> 
-        <Route path="/" component={ ()=> <>asda </>} exact />
+    <Switch>                                 
+        <Route path="/" component={ ()=> <>asda </>} exact /> 
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/tarefas" component={Tarefas} />
     </Switch>
